8c1d8fb1d262ebe350bd467df57ace9e61cbcc20
EduardMarkgraf <Eduard.Markgraf@consiliari.de>
2025-02-07 03:19:25 +0100

    Remove unused Config.cs

--- Migrating Entities in a Poorly Designed Multi-Tenant System/RealWorldSolution/RealWorldSolution/Migration/Config.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace RealWorldSolution.Migration;
-
-public class Config
-{
-    public required Guid SourceLawOfficeId { get; init; }
-    public required Guid DestinationLawOfficeId { get; init; }
-}
